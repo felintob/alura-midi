cbedba88fe9ce092e2a4755f764c6cbdfa0deaf0
felintob <bfelinto@gmail.com>
2022-05-24 18:57:30 -0300

    Implementação do código javascript para o acesso ao som

---
+++ main.js
@@ -0,0 +1,18 @@
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play()
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador]
+  const instrumento = tecla.classList[1]
+  const idAudio = `#som_${instrumento}`
+
+  tecla.onClick = () => {
+    tocaSom(idAudio)
+  }
+  contador += 1
+}
